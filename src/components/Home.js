7a9228d09d8e645b8f4884889624fd8447152545
Gloria <naomiegloria507@gmail.com>
2024-07-25 12:26:10 +0200

    first react lesson

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+    return(
+        <div>
+            <h1>
+                login
+            </h1>
+<labele label for="password">password</labele>
+<input type="password"/>
+<labele label for="email"></labele>
+<input type="email"/>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+function Login (){
+
+    return(
+        <div>
+        <h1>
+            login
+        </h1>
+<label for="password">password</label>
+<input type="password"/>
+<label for="email">email</label>
+<input type="email"/>
+    </div>
+    )
+}
+export default  Login
